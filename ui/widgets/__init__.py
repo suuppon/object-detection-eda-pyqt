ae4d3c27d59c4074ef2cc30e0408d58880a78967
suuppon <albert1226@g.skku.edu>
2025-12-02 16:31:37 +0900

    refactor: add widgets module __init__ for easier imports

---
+++ ui/widgets/__init__.py
@@ -0,0 +1,29 @@
+from .advanced_widget import AdvancedStatsWidget
+from .difficulty_widget import DifficultyWidget
+from .duplicate_widget import DuplicateWidget
+from .geometry_widget import GeometryWidget
+from .guide_widget import GuideWidget
+from .health_widget import HealthCheckWidget
+from .quality_widget import ImageQualityWidget
+from .relation_widget import RelationWidget
+from .signal_widget import SignalAnalysisWidget
+from .spatial_widget import SpatialWidget
+from .stat_widget import StatWidget
+from .strategy_widget import StrategyWidget
+from .viewer_widget import ImageViewer
+
+__all__ = [
+    "AdvancedStatsWidget",
+    "DifficultyWidget",
+    "DuplicateWidget",
+    "GeometryWidget",
+    "GuideWidget",
+    "HealthCheckWidget",
+    "ImageQualityWidget",
+    "RelationWidget",
+    "SignalAnalysisWidget",
+    "SpatialWidget",
+    "StatWidget",
+    "StrategyWidget",
+    "ImageViewer",
+]
